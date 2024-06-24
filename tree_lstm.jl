48c59714904194a4478ee542d1410b81a4b1e9fd
superrandomusernamewhichisnotreal <superrandomemailwhichisnotreal@example.com>
2024-06-23 20:06:27 -0400

    changing the lstm encoding and passing to the model

--- tree_lstm.jl
+++ tree_lstm.jl
@@ -1,15 +1,25 @@
+include("src/MyModule.jl")
+using BenchmarkTools
+using .MyModule: load_data, preprosses_data_to_expressions
 using Metatheory
 using Statistics
 using Flux
+using Mill
 #: Dense, sigmoid, tanh, relu, \sigmoid
 
+#=
 struct MyNodes#{C, E<:Vector{UInt32}}
     children::Union{Vector{MyNodes}, Nothing}
     #children::C
     encoding::Vector{UInt32}
     #encoding::E
 end
-
+=#
+struct ExprEncoding{B, BI, PM}
+    buffer::B
+    bag_ids::BI
+    parent_map::PM
+end
 # Define the TreeLSTM model
 mutable struct TreeLSTM
     W_i::Dense
@@ -34,7 +44,7 @@ TreeLSTM(in_dim::Int, out_dim::Int) = TreeLSTM(
 )
 
 
-function (lstm::TreeLSTM)(x::Vector{UInt32}, c::Vector{Float32}, h::Vector{Float32})
+function (lstm::TreeLSTM)(x::Vector{Float32}, c::Vector{Float32}, h::Vector{Float32})
     i = sigmoid.(lstm.W_i(x) .+ lstm.U_i(h))
     f = sigmoid.(lstm.W_f(x) .+ lstm.U_f(h))
     o = sigmoid.(lstm.W_o(x) .+ lstm.U_o(h))
@@ -44,7 +54,17 @@ function (lstm::TreeLSTM)(x::Vector{UInt32}, c::Vector{Float32}, h::Vector{Float
     return c_next, h_next
 end
 
-@inline function tree_lstm(lstm::TreeLSTM, tree::MyNodes, c::Vector{Float32}, h::Vector{Float32})
+function (lstm::TreeLSTM)(x::Matrix{Float32}, c::Matrix{Float32}, h::Matrix{Float32})
+    i = sigmoid.(lstm.W_i(x) .+ lstm.U_i(h))
+    f = sigmoid.(lstm.W_f(x) .+ lstm.U_f(h))
+    o = sigmoid.(lstm.W_o(x) .+ lstm.U_o(h))
+    u = tanh.(lstm.W_u(x) .+ lstm.U_u(h))
+    c_next = f .* c .+ i .* u
+    h_next = o .* tanh.(c_next)
+    return c_next, h_next
+end
+
+@inline function tree_lstm(lstm::TreeLSTM, tree::Dict, c::Vector{Float32}, h::Vector{Float32})
     if isnothing(tree.children)
         data = tree.encoding
         return lstm(data, c, h)
@@ -73,15 +93,30 @@ LikelihoodModel(input_size::Int, hidden_size::Int, mlp_hidden_size::Int) = Likel
     Dense(mlp_hidden_size, 1, σ)
 )
 
-function (m::LikelihoodModel)(tree::MyNodes, c_init::Vector{Float32}=zeros(Float32, size(m.fc.weight)[2]), h_init::Vector{Float32}=zeros(Float32, size(m.fc.weight)[2]))::Float32
+function (m::LikelihoodModel)(tree::Dict, c_init::Vector{Float32}=zeros(Float32, size(m.fc.weight)[2]), h_init::Vector{Float32}=zeros(Float32, size(m.fc.weight)[2]))::Float32
     h, _ = tree_lstm(m.tree_lstm, tree, c_init, h_init)
     h = m.fc(h)
     output = m.output_layer(h)
     return output[1]
 end
 
+function (m::LikelihoodModel)(buffer::Dict, parent_map::Dict, bag_ids::Dict)
+    h, _ = tree_lstm1(m.tree_lstm, buffer, parent_map, bag_ids)
+    h = m.fc(h)
+    output = m.output_layer(h)
+    return output[1]
+end
+
+
+function (m::LikelihoodModel)(expr_encoder::ExprEncoding)
+    buffer, bag_ids, parent_map = expr_encoder.buffer, expr_encoder.bag_ids, expr_encoder.parent_map
+    h, _ = tree_lstm1(m.tree_lstm, buffer, parent_map, bag_ids)
+    h = m.fc(h)
+    output = m.output_layer(h)
+    return output[1]
+end
 # Initialize model and example tree
-all_symbols = [:+, :-, :/, :*, :<=, :>=, :min, :max, :<, :>, :select]
+all_symbols = [:+, :-, :/, :*, :<=, :>=, :min, :max, :<, :>, :select, :&&]
 in_dim = length(all_symbols) + 2
 symbols_to_index = Dict(i=>ind for (ind, i) in enumerate(all_symbols))
 out_dim = in_dim * 2
@@ -92,86 +127,243 @@ ex = :(a - b + c)
 #symbol_encoding(sym::Symbol) = sym in all_symbols ? ones(length(all_symbols) + 2) * symbols_to_index[sym] : ones(length(all_symbols) + 2) * -2
 #symbol_encoding(sym) = ones(length(all_symbols) + 2) * -1
 
-function expression_encoder!(ex::Union{Expr, Symbol, Int64}, all_symbols::Vector{Symbol}, symbols_to_index::Dict{Symbol, Int64}, depth::Int, buffer::Dict{Int, Vector}, bag_ids::Vector)
+
+
+function ex2mill(ex::Expr, symbols_to_index)
+    if ex.head == :call
+        fun_name, args =  ex.args[1], ex.args[2:end]
+    elseif ex.head == Symbol("&&")
+        fun_name = ex.head
+        args = ex.args
+    else
+        error("unknown head $(ex.head)")
+    end
+    n = length(symbols_to_index) + 2
+    ds = ProductNode((
+        head = ArrayNode(Flux.onehotbatch([symbols_to_index[fun_name]], 1:n)),
+        args = args2mill(args, symbols_to_index)
+    ))
+    return(ds)
+end
+
+function ex2mill(ex::Symbol, symbols_to_index)
+    n = length(symbols_to_index) + 2
+    ds = ProductNode((
+        head = ArrayNode(Flux.onehotbatch([n], 1:n)),
+        args = BagNode(missing, [0:-1])
+        ))
+end
+
+function ex2mill(ex::Number, symbols_to_index)
+    n = length(symbols_to_index) + 2
+    ds = ProductNode((
+        head = ArrayNode(Flux.onehotbatch([n-1], 1:n)),
+        args = BagNode(missing, [0:-1])
+        ))
+end
+
+function args2mill(args::Vector, symbols_to_index)
+    isempty(args) && return(BagNode(missing, [0:-1]))
+    BagNode(
+        reduce(catobs, map(a -> ex2mill(a, symbols_to_index), args)),
+        [1:length(args)]
+        )
+end
+
+
+struct ExprModel{HM,A,JM}
+    head_model::HM
+    aggregation::A
+    joint_model::JM    
+end
+
+
+function (m::ExprModel)(ds::ProductNode)
+    tmp = vcat(m.head_model(ds.data.head.data), m(ds.data.args))
+    println(size(tmp))
+    m.joint_model(tmp)
+end
+
+function (m::ExprModel)(ds::BagNode)
+    m.aggregation(m(ds.data), ds.bags)
+end
+
+(m::ExprModel)(ds::Missing) = missing
+
+
+#=
+data = load_data("data/test.json")[1:100]
+data = preprosses_data_to_expressions(data)
+
+hidden_size = 8
+m = ExprModel(
+    Flux.Chain(Dense(length(symbols_to_index) + 2, hidden_size,relu), Dense(hidden_size,hidden_size)),
+    Mill.SegmentedSumMax(7),
+    Flux.Chain(Dense(3*hidden_size, hidden_size,relu), Dense(hidden_size,1))
+    )
+=#
+#dss = [ex2mill(ex, symbols_to_index) for ex in data[1:10]]
+#@benchmark [ex2mill(ex, symbols_to_index) for ex in data[1:10]]
+#ds = reduce(catobs, dss)
+#reduce(hcat, map(m, dss)) ≈ m(ds)
+#m(ex2mill(:(a-a), symbols_to_index))
+
+function expression_encoder!(ex::Union{Expr, Symbol, Int64}, all_symbols::Vector{Symbol}, symbols_to_index::Dict{Symbol, Int64}, depth::Int, buffer::Dict{Int, Vector}, bag_ids::Dict{Int, Vector}, parent_id::Int, parent_map::Dict{Int,Int})
     if ex isa Expr
-        encoded_symbol_root = zeros(length(all_symbols) + 2)
-        encoded_symbol_root[symbols_to_index[ex.args[1]]] = 1
-        if depth == 1
-            buffer[depth] = Vector[encoded_symbol_root]
+        encoded_symbol_root = zeros(Float32, length(all_symbols) + 2)
+        if ex.head != :call
+            encoded_symbol_root[symbols_to_index[ex.head]] = 1
+        else
+            encoded_symbol_root[symbols_to_index[ex.args[1]]] = 1
         end
+        node_id = length(parent_map) + 1
+        parent_map[node_id] = parent_id
+
+        if depth == 1
+            buffer[depth] = [encoded_symbol_root]
+            bag_ids[depth] = [node_id]
+        else
+            if haskey(buffer, depth)
+                push!(buffer[depth], encoded_symbol_root)
+                push!(bag_ids[depth], node_id)
+            else
+                buffer[depth] = [encoded_symbol_root]
+                bag_ids[depth] = [node_id]
+            end
+        end
+
         depth += 1
-        #push!(bag_ids, depth - 1 + )
-        encoded_symbol_first_child = expression_encoder!(ex.args[2], all_symbols, symbols_to_index, depth, buffer, bag_ids)
-        #encoded_symbol_first_child = symbol_encoding(ex.args[2])
+        encoded_symbol_first_child = expression_encoder!(ex.args[2], all_symbols, symbols_to_index, depth, buffer, bag_ids, node_id, parent_map)
+        #=
         if haskey(buffer, depth)
             push!(buffer[depth], encoded_symbol_first_child)
+            push!(bag_ids[depth], node_id+1)
         else
-            buffer[depth] = Vector[encoded_symbol_first_child]
+            buffer[depth] = [encoded_symbol_first_child]
+            bag_ids[depth] = [node_id+1]
         end
+        =#
         if length(ex.args) == 3
-            #push!(bag_ids, depth - 1)
-            encoded_symbol_second_child = expression_encoder!(ex.args[3], all_symbols, symbols_to_index, depth, buffer, bag_ids)
-            push!(buffer[depth], encoded_symbol_second_child)
+            encoded_symbol_second_child = expression_encoder!(ex.args[3], all_symbols, symbols_to_index, depth, buffer, bag_ids, node_id, parent_map)
+            #push!(buffer[depth], encoded_symbol_second_child)
+            #push!(bag_ids[depth], node_id+2)
         end
         return encoded_symbol_root
     elseif ex isa Symbol && ex in all_symbols
-        encoded_symbol = zeros(length(all_symbols) + 2)
-        encoded_symbol[symbols_to_index[ex]] = 1
+        encoded_symbol = zeros(Float32, length(all_symbols) + 2)
+        encoded_symbol[symbols_to_index[ex]] = 1 
+        node_id = length(parent_map) + 1
+        parent_map[node_id] = parent_id
+        if !haskey(bag_ids, depth)
+            buffer[depth] = [encoded_symbol]
+            bag_ids[depth] = [node_id]
+        else
+            push!(buffer[depth], encoded_symbol)
+            push!(bag_ids[depth], node_id)
+        end
         return encoded_symbol
     elseif ex isa Symbol 
-        encoded_symbol = zeros(length(all_symbols) + 2)
+        encoded_symbol = zeros(Float32, length(all_symbols) + 2)
         encoded_symbol[end] = 1
+        node_id = length(parent_map) + 1
+        parent_map[node_id] = parent_id
+        if !haskey(bag_ids, depth)
+            buffer[depth] = [encoded_symbol]
+            bag_ids[depth] = [node_id]
+        else
+            push!(buffer[depth], encoded_symbol)
+            push!(bag_ids[depth], node_id)
+        end
         return encoded_symbol
     else
-        encoded_symbol = zeros(length(all_symbols) + 2)
+        encoded_symbol = zeros(Float32, length(all_symbols) + 2)
         encoded_symbol[end - 1] = 1
+        node_id = length(parent_map) + 1
+        parent_map[node_id] = parent_id
+        if !haskey(bag_ids, depth)
+            buffer[depth] = [encoded_symbol]
+            bag_ids[depth] = [node_id]
+        else
+            push!(buffer[depth], encoded_symbol)
+            push!(bag_ids[depth], node_id)
+        end
         return encoded_symbol
     end
 end
 
-
-buffer = Dict{Int, Vector}()
-bag_ids = Vector()
-expression_encoder!(ex, all_symbols, symbols_to_index, 1, buffer, bag_ids)
-#@assert length(buffer[6]) == 2
-#@assert length(buffer[5]) == 4
-#=
-loss(model::LikelihoodModel, expanded_nodes::AbstractVector, not_expanded_nodes::Nothing) = log(1 + exp(sum(model.(expanded_nodes))))
-function loss(model::LikelihoodModel, expanded_nodes::AbstractVector, not_expanded_nodes::AbstractVector)
-  return log(1 + exp(sum(model.(expanded_nodes)) - sum(model.(not_expanded_nodes))))
+@inline function tree_lstm1(lstm::TreeLSTM, buffer::Dict, parent_map::Dict, bag_ids::Dict)
+    h_tmp, c_tmp = lstm(buffer[1][1], zeros(Float32, out_dim), zeros(Float32, out_dim))
+    return h_tmp, c_tmp
+    #=
+    all_nodes = length(parent_map)
+    all_depth = length(buffer)
+    out_dim = size(lstm.W_i.weight, 1)
+    if all_nodes == 1
+        h_tmp, c_tmp = lstm(buffer[1][1], zeros(Float32, out_dim), zeros(Float32, out_dim))
+        return h_tmp, c_tmp
+    end
+    #c_init = zeros(Float32, out_dim, length(buffer[all_depth]))
+    #h_init = zeros(Float32, out_dim, length(buffer[all_depth]))
+    #println(buffer)
+    c_tmp = zeros(Float32, out_dim, length(buffer[all_depth]))
+    h_tmp = zeros(Float32, out_dim, length(buffer[all_depth]))
+    while true
+        x = hcat(buffer[all_depth]...)
+        all_depth -= 1
+        c_tmp, h_tmp = lstm(x, c_tmp, h_tmp) 
+        if all_depth == 0
+            return h_tmp, c_tmp
+        end
+        c_new = zeros(Float32, out_dim, length(buffer[all_depth]))
+        h_new = zeros(Float32, out_dim, length(buffer[all_depth]))
+        for (node_index, node_id) in enumerate(bag_ids[all_depth + 1])
+            parent_index = findall(x-> x == parent_map[node_id], bag_ids[all_depth])[1]
+            if parent_index !== nothing
+                c_new[:, parent_index] += c_tmp[:, node_index]
+                h_new[:, parent_index] += h_tmp[:, node_index]
+            end
+        end
+        c_tmp = c_new
+        h_tmp = h_new
+    end
+    =#
 end
 
-function loss(model::LikelihoodModel, first_any::Vector, second_any::Vector)
-    my_loss = Float32[
-        log(1 + exp(model(i) - model(j)))
-        for (ind, i) in enumerate(first_any)
-        for j in second_any[ind]
-    ]
-    return mean(my_loss)
-end
-=#
-function lgbf_loss(model, h_pos, h_neg)
-  #return isempty(h_neg) ? mean(log(1 + exp(model(h_pos)))) : mean(log.(1 .+ exp.(model(h_pos) .- model.(h_neg))))
-  return mean(log(1 + exp(model(h_pos))))
-end
 
-
-function loss(model::LikelihoodModel, depth_dict::Dict{Int, Vector}, c_init::Vector{Float32}, h_init::Vector{Float32})
-    #result = [lgbf_loss(model, i, second_any[ind])
-              #for (ind, i) in enumerate(first_any)]
-    #println(first_any)
-    #println(length.(second_any))
-   # return mean(result)
-    #return mean(model(first_any[1]) .- model.(second_any[1]))
-  #return mean(log.(1 .+ exp.(model.(first_any))))
-  #return mean(log.(1 .+ exp.(model.(first_any, c_init, h_init))))
-  #return mean([model(i, c_init, h_init) for i in first_any])
-    return 1
+#mod2 = LikelihoodModel(in_dim, out_dim, out_dim * 2)
+#for ex in data
+function expression_encoder(ex::Union{Expr, Int}, all_symbols::Vector{Symbol}, symbols_to_index::Dict{Symbol, Int})::ExprEncoding
+    buffer = Dict{Int, Vector}()
+    bag_ids = Dict{Int, Vector}()
+    parent_map = Dict{Int, Int}()
+    #expression_encoder!(ex, all_symbols, symbols_to_index, 1, buffer, bag_ids, 1, parent_map)
+    x = expression_encoder!(ex, all_symbols, symbols_to_index, 1, buffer, bag_ids, 1, parent_map)
+    if isempty(buffer)
+        buffer[1] = x 
+        bag_ids[1] = [1]
+    end
+    return ExprEncoding(buffer, bag_ids, parent_map)  
 end
 
-#model = LikelihoodModel(in_dim, out_dim, out_dim * 2)
+function loss(model::LikelihoodModel, depth_dict::Dict{Int, Vector})
+    return model(depth_dict[1][1]) 
+end
 
-
-#result_prob = model(expression_encoder!(ex))
-#println("Final result: $result_prob")
-
+#function loss(model::LikelihoodModel, depth_dict::Dict{Int, Vector})
+    #=
+    result = []
+    for i in length(depth_dict):-1:1 
+        expanded_node_prob = model(depth_dict[i][1])
+        not_expanded_node_prob = model.(depth_dict[i][2:end])
+        lgbf_loss = mean(log.(1 .+ exp.(expanded_node_prob .- not_expanded_node_prob)))
+        push!(result, lgbf_loss)
+    end
+    return mean(result) 
+    =#
+    #return mean([mean(log.(1 .+ exp.(model(depth_dict[i][1]) .- model.(depth_dict[i][2:end])))) for i in length(depth_dict):-1:1])
+    #return model(depth_dict[1][1])
+    #expr_encoder = depth_dict[1][1]
+    #buffer, bag_ids, parent_map = expr_encoder.buffer, expr_encoder.bag_ids, expr_encoder.parent_map
+    #output = model(buffer, parent_map, bag_ids)
+    #return mean(log.(1 .+ exp.(output)))
+#end
--- tree_simplifier.jl
+++ tree_simplifier.jl
@@ -64,12 +64,12 @@ mutable struct Node{E, P}
     depth::Int64
     node_id::UInt64
     #expression_encoding::Union{MyNodes, Nothing}
-    expression_encoding::MyNodes
+    expression_encoding::Union{ExprEncoding, ProductNode}
 end
 
 Node(ex::Int, rule_index, parent, depth, ee) = Node(ex, rule_index, UInt64[],  parent, depth, hash(ex), ee)
 
-function Node(ex::Expr, rule_index::Int64, parent::UInt64, depth::Int64, ee::MyNodes)
+function Node(ex::Expr, rule_index::Int64, parent::UInt64, depth::Int64, ee::Union{ExprEncoding, ProductNode})
     Node(ex, rule_index, UInt64[],  parent, depth, hash(ex), ee)
 end
 
@@ -89,7 +89,7 @@ function push_to_tree!(soltree::Dict, new_node::Node)
     node_id = new_node.node_id
     if haskey(soltree, node_id)
         old_node = soltree[node_id]
-        soltree[node_id] = new_node.depth < old_node.depth ? new_node : old_node
+        #soltree[node_id] = new_node.depth < old_node.depth ? new_node : old_node
         return (false)
     else
         soltree[node_id] = new_node
@@ -98,18 +98,20 @@ function push_to_tree!(soltree::Dict, new_node::Node)
 end
 
 
-function expand_node!(parent::Node, soltree::Dict{UInt64, Node}, heuristic::LikelihoodModel, open_list::PriorityQueue{Node,Float32, Base.Order.ForwardOrdering}, encodings_buffer::Dict{UInt64, MyNodes}, all_symbols::Vector{Symbol}, symbols_to_index::Dict{Symbol, Int64}, c_init::Vector{Float32}, h_init::Vector{Float32})::Nothing
+function expand_node!(parent::Node, soltree::Dict{UInt64, Node}, heuristic::LikelihoodModel, open_list::PriorityQueue{Node,Float32, Base.Order.ForwardOrdering}, encodings_buffer::Dict{UInt64, ExprEncoding}, all_symbols::Vector{Symbol}, symbols_to_index::Dict{Symbol, Int64}, c_init::Vector{Float32}, h_init::Vector{Float32})::Nothing
     ex = parent.ex
     succesors = filter(r -> r[2] != ex, collect(enumerate(execute(ex, theory))))
 
     for (rule_index, new_exp) in succesors
         if !haskey(encodings_buffer, hash(new_exp))
-            encodings_buffer[hash(new_exp)] = expression_encoder!(new_exp, all_symbols, symbols_to_index)
+            encodings_buffer[hash(new_exp)] = expression_encoder(new_exp, all_symbols, symbols_to_index)
+            #encodings_buffer[hash(new_exp)] = ex2mill(new_exp, symbols_to_index)
         end
         new_node = Node(new_exp, rule_index, parent.node_id, parent.depth + 1, encodings_buffer[hash(new_exp)])
         if push_to_tree!(soltree, new_node)
             push!(parent.children, new_node.node_id)
-            enqueue!(open_list, new_node, heuristic(new_node.expression_encoding, c_init, h_init))
+            #println(new_exp)
+            enqueue!(open_list, new_node, heuristic(new_node.expression_encoding))
         end
     end
 end
@@ -121,30 +123,10 @@ function prity_print(arguments)
   end
 end
 
-function get_not_expanded_nodes1(expanded_node_depth, open_list, encodings_buffer)
-    result = MyNodes[] 
-    for (i, number) in open_list
-        if i.depth == expanded_node_depth
-            push!(result, encodings_buffer[i.ex]) 
-        end
-    end
-    return result
-end
 
-function get_not_expanded_nodes(expanded_node::Node, soltree::Dict{UInt64, Node}, encodings_buffer::Dict{UInt64, MyNodes})
-    result = MyNodes[] 
-    for i in soltree[expanded_node.parent].children
-        if i != expanded_node.node_id
-            not_expanded_nodes = soltree[i].node_id
-            push!(result, encodings_buffer[not_expanded_nodes])
-        end
-    end
-    return result
-end
-
-function build_tree!(soltree::Dict{UInt64, Node}, heuristic::LikelihoodModel, open_list::PriorityQueue{Node,Float32, Base.Order.ForwardOrdering}, close_list::Set{UInt64}, encodings_buffer::Dict{UInt64, MyNodes}, all_symbols::Vector{Symbol}, symbols_to_index::Dict{Symbol, Int64}, c_init::Vector{Float32}, h_init::Vector{Float32}) 
+function build_tree!(soltree::Dict{UInt64, Node}, heuristic::LikelihoodModel, open_list::PriorityQueue{Node,Float32, Base.Order.ForwardOrdering}, close_list::Set{UInt64}, encodings_buffer::Dict{UInt64, ExprEncoding}, all_symbols::Vector{Symbol}, symbols_to_index::Dict{Symbol, Int64}, c_init::Vector{Float32}, h_init::Vector{Float32}) 
     #expanded_nodes = MyNodes[]
-    max_steps = 500
+    max_steps = 1000
     #not_expanded_nodes = Vector[]
     while length(open_list) > 0
         if max_steps == 0
@@ -152,7 +134,7 @@ function build_tree!(soltree::Dict{UInt64, Node}, heuristic::LikelihoodModel, op
         end
         max_steps -= 1
         node = dequeue!(open_list)
-        if node.depth >= 5
+        if node.depth >= 15
             continue
         end
         expand_node!(node, soltree, heuristic, open_list, encodings_buffer, all_symbols, symbols_to_index, c_init, h_init)
@@ -161,17 +143,6 @@ function build_tree!(soltree::Dict{UInt64, Node}, heuristic::LikelihoodModel, op
         if node.depth == 0
             continue
         end
-        #push!(expanded_nodes, encodings_buffer[node.node_id])
-        #push!(not_expanded_nodes, get_not_expanded_nodes1(node.depth, open_list, encodings_buffer))
-        #push!(not_expanded_nodes, get_not_expanded_nodes(node, soltree, encodings_buffer))
-        #heuristic(node.expression_encoding, c_init, h_init)
-        #loss(heuristic, expanded_nodes, not_expanded_nodes, c_init, h_init)
-        #=
-        grad = gradient(pc) do 
-          loss(heuristic, expanded_nodes, not_expanded_nodes, c_init, h_init)
-        end
-        Flux.update!(optimizer, pc, grad)
-        =#
     end
 end
 
@@ -187,37 +158,28 @@ end
 
 function extract_rules_applied1(node::Node, soltree::Dict{UInt64, Node}) 
     proof_vector = Vector()
-    depth_dict = Dict{Int, Vector}
+    depth_dict = Dict{Int, Vector}()
     #while !isnothing(node.parent)
     #nodes_to_use = filter(n->n.depth<node.depth, values(soltree)
     while node.parent != node.node_id
         push!(proof_vector, node.rule_index)
-        depth_dict[node.depth] = Vector[node.expression_encoding]
-        for children_ids in soltree[node.parent]
-            if children_ids.node_id == node.node_id
+        depth_dict[node.depth] = [node.expression_encoding]
+        for children_id in soltree[node.parent].children
+            if children_id == node.node_id
                 continue
             end
-            push!(depth_dict[node.depth], soltree[children_ids].expression_encoding)
+            push!(depth_dict[node.depth], soltree[children_id].expression_encoding)
         end
         node = soltree[node.parent]
     end
     return reverse(proof_vector), depth_dict
 end
+
 function extract_smallest_terminal_node(soltree::Dict{UInt64, Node}, close_list::Set{UInt64})
-    #result = soltree[close_list[1]]
     all_nodes = [i for i in values(soltree)] 
     exs = [i.ex for i in values(soltree)] 
     smallest_expression_node = argmin(exp_size.(exs))
     return all_nodes[smallest_expression_node]
-    #=
-    result = nothing
-    for node_hash in close_list
-        if isnothing(result) || (exp_size(result.ex) >= exp_size(soltree[node_hash].ex) && result.depth > soltree[node_hash].depth)
-            result = soltree[node_hash]
-        end
-    end
-    return result
-    =#
 end
 
 heuristic = LikelihoodModel(in_dim, out_dim, out_dim * 2)
@@ -233,33 +195,37 @@ depth = 50
 
 pc = Flux.params(heuristic)
 ex = data[3]
-# Todo: fix issues with && and ||
 
-#for (index, ex) in enumerate(data[1:10])
-soltree = Dict{UInt64, Node}()
-open_list = PriorityQueue{Node, Float32}()
-close_list = Set{UInt64}()
-encodings_buffer = Dict{UInt64, MyNodes}()
-#encodings_buffer = Dict()
-println("Initial expression: $ex")
-encoded_ex = expression_encoder!(ex, all_symbols, symbols_to_index)
-root = Node(ex, 0, hash(ex), 0, encoded_ex)
-soltree[root.node_id] = root
-#push!(open_list, root.node_id)
-enqueue!(open_list, root, heuristic(root.expression_encoding))
+for (index, ex) in enumerate(data)
+    soltree = Dict{UInt64, Node}()
+    open_list = PriorityQueue{Node, Float32}()
+    close_list = Set{UInt64}()
+    encodings_buffer = Dict{UInt64, ExprEncoding}()
+    #encodings_buffer = Dict()
+    println("Initial expression: $ex")
+    encoded_ex = expression_encoder(ex, all_symbols, symbols_to_index)
+    #encoded_ex = ex2mill(ex, symbols_to_index)
+    root = Node(ex, 0, hash(ex), 0, encoded_ex)
+    soltree[root.node_id] = root
+    #push!(open_list, root.node_id)
+    enqueue!(open_list, root, heuristic(root.expression_encoding))
 
-build_tree!(soltree, heuristic, open_list, close_list, encodings_buffer, all_symbols, symbols_to_index, c_init, h_init)
-println("Have successfuly finished bulding simplification tree!")
+    build_tree!(soltree, heuristic, open_list, close_list, encodings_buffer, all_symbols, symbols_to_index, c_init, h_init)
+    println("Have successfuly finished bulding simplification tree!")
 
-smallest_node = extract_smallest_terminal_node(soltree, close_list)
-simplified_expression = smallest_node.ex
-println("Simplified expression: $simplified_expression")
+    smallest_node = extract_smallest_terminal_node(soltree, close_list)
+    simplified_expression = smallest_node.ex
+    println("Simplified expression: $simplified_expression")
 
-proof_vector, depth_dict = extract_rules_applied1(smallest_node, soltree)
-#proof_vector = extract_rules_applied(smallest_node, soltree)
-println("Proof vector: $proof_vector")
-grad = gradient(pc) do 
-    loss(heuristic, depth_dict, c_init, h_init)
+    proof_vector, depth_dict = extract_rules_applied1(smallest_node, soltree)
+    #proof_vector = extract_rules_applied(smallest_node, soltree)
+    if isempty(proof_vector)
+        continue
+    end
+    println("Proof vector: $proof_vector")
+    grad = gradient(pc) do 
+        #println("Loss: $(loss(heuristic, depth_dict))")
+        loss(heuristic, depth_dict)
+    end
+    Flux.update!(optimizer, pc, grad)
 end
-Flux.update!(optimizer, pc, grad)
-#end
