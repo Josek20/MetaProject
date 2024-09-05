6273b9a73d6d210ccfc473eb053951bf6b8eb2bc
superrandomusernamewhichisnotreal <superrandomemailwhichisnotreal@example.com>
2024-09-05 05:23:03 -0400

    changed tree embedding

--- src/tree_embedding.jl
+++ src/tree_embedding.jl
@@ -15,9 +15,9 @@ function ex2mill(ex::Expr, symbols_to_index, all_symbols, position)
     # encoding = Flux.onehotbatch([symbols_to_index[fun_name]], 1:n) * position
     # encoding = Flux.onehotbatch([1, symbols_to_index[fun_name]], 1:n)
     encoding = Flux.onehotbatch([symbols_to_index[fun_name]], 1:n)
-    emb_ind = popfirst!(position)
-    pos_encoding = [parse(Int, i) for i in bitstring(Int16(emb_ind))]
-    encoding = vcat(encoding, pos_encoding)
+    # emb_ind = popfirst!(position)
+    # pos_encoding = [parse(Int, i) for i in bitstring(Int16(0))]
+    # encoding = vcat(encoding, pos_encoding)
     ds = ProductNode((
         head = ArrayNode(encoding),
         args = args2mill(args, symbols_to_index, all_symbols, position)
@@ -29,9 +29,9 @@ function ex2mill(ex::Symbol, symbols_to_index, all_symbols, position)
     n = length(symbols_to_index) + 2
     # encoding = Flux.onehotbatch([n-1], 1:n) * position
     encoding = Flux.onehotbatch([n-1], 1:n)
-    emb_ind = popfirst!(position)
-    pos_encoding = [parse(Int, i) for i in bitstring(Int16(emb_ind))]
-    encoding = vcat(encoding, pos_encoding)
+    # emb_ind = popfirst!(position)
+    # pos_encoding = [parse(Int, i) for i in bitstring(Int16(0))]
+    # encoding = vcat(encoding, pos_encoding)
     # encoding = Flux.onehotbatch([n-1], 1:n)
     ds = ProductNode((
         head = ArrayNode(encoding),
@@ -44,9 +44,9 @@ function ex2mill(ex::Number, symbols_to_index, all_symbols, position)
     # encoding = Flux.onehotbatch([n-2], 1:n) * position
     # @show ex
     encoding = Flux.onehotbatch([n-2], 1:n) # * Float32(ex)
-    emb_ind = popfirst!(position)
-    pos_encoding = [parse(Int, i) for i in bitstring(Int16(emb_ind))]
-    encoding = vcat(encoding, pos_encoding)
+    # emb_ind = popfirst!(position)
+    # pos_encoding = [parse(Int, i) for i in bitstring(Int16(ex))]
+    # encoding = vcat(encoding, pos_encoding)
     # encoding = Flux.onehotbatch([n-2], 1:n)
     ds = ProductNode((
         head = ArrayNode(encoding),
@@ -92,19 +92,39 @@ end
 #      
 # end
 function (m::ExprModel)(ds::ProductNode)
-    m.heuristic(embed(m, ds))
+    m.heuristic(embed(m, ds, zeros(1, size(ds.data.head.data)[2])))
 end
 
-function embed(m::ExprModel, ds::ProductNode)
-    tmp = vcat(m.head_model(ds.data.head.data), embed(m, ds.data.args))
+function embed(m::ExprModel, ds::ProductNode, pos_em::Matrix)
+    # sds = size(ds.data.head.data)[2]
+    # pos_emb = repeat([1,2], sds ÷ 2)#[collect(1:2) for _ in 1:sds ÷ 2]
+    # # pos_emb = hcat([bitstring(Int16(i)) for i in collect(1:sds)]...)
+    # # if isempty(pos_emb)
+    # #     pos_emb = 
+    # # end
+    # @show pos_emb
+    # @show sds
+    # reshaped = isempty(pos_emb) ? zeros(1, sds) : reshape(pos_emb, (1, sds))
+    # @show size(reshaped)
+    # @show size(ds.data.head.data)
+    # stmp = vcat(ds.data.head.data, pos_em)
+    # @show size(stmp)
+    # tmp1, tmp2 = m.head_model(stmp), embed(m, ds.data.args, pos_em)
+    # @show tmp1
+    # tmp = vcat(tmp1, tmp2)
+    tmp = vcat(m.head_model(ds.data.head.data), embed(m, ds.data.args, pos_em))
+    # @show size(tmp)
+    # @show ds.data.head.data
+    # @show tmp2
     m.joint_model(tmp)
 end
 
-function embed(m::ExprModel, ds::BagNode)
-    m.aggregation(embed(m, ds.data), ds.bags)
+function embed(m::ExprModel, ds::BagNode, pos_em::Matrix)
+    tmp = embed(m, ds.data, pos_em)
+    m.aggregation(tmp, ds.bags)
 end
 
-embed(m::ExprModel, ds::Missing) = missing
+embed(m::ExprModel, ds::Missing, pos_em::Matrix) = missing
 
 function loss(heuristic, big_vector, hp=nothing, hn=nothing)
     o = heuristic(big_vector) 
@@ -114,6 +134,9 @@ function loss(heuristic, big_vector, hp=nothing, hn=nothing)
     filtered_diff = filter(x-> x != 0, diff)
     return sum(log.(1 .+ exp.(diff)))
 end
+
+# :(min(min(v1 * 4, 67) + v0 * 71, 137) <= (1018 + v1 * 4) + v0 * 71)
+# :(min(min(v1 * 4, 67) + v0 * 71, 137) <= (v0 * 71 + 1018) + v1 * 4)
 
 function heuristic_loss(heuristic, data, in_solution, not_in_solution)
     loss_t = 0.0
